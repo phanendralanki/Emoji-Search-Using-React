a5f203b2c50036984c8e0b684bdf17ce2b722d2c
lanki Ganesh phanendra <105139595+phanendralanki@users.noreply.github.com>
2023-07-31 21:07:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,63 @@
+// import logo from './logo.svg';
+// import './components/Greet'
+
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+
+
+
+import './App.css';
+
+import ArrayList from './components/ArrayList';
+
+// import ClassClickCode from './components/ClassClickCode';
+
+import EmojiSearch from './components/EmojiSearch';
+
+// import FunctionClick from './components/FunctionClick';
+
+
+// import Message from './components/Message';
+
+// import Calc from './funCalculator/Calc';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    {/* <h1>welcome</h1> */}
+    {/* <Greet name="Rajpaul" surname="Maddala"/> */}
+    {/* <Greet name="charan" surname="Mutyala"/>
+    <Greet name="phani"/> */}
+
+    
+    {/* <Welcome name="rajpaul"/> */}
+
+    {/* state */}
+    {/* <Message/> */}
+    {/* state */}
+
+
+    {/* Calculator */}
+    {/* <Calc/> */}
+    {/* Calculator */}
+
+    {/* <FunctionClick/> */}
+    {/* <ClassClickCode/> */}
+
+
+    {/* arrays */}
+    <ArrayList/>
+
+
+    {/* Emoji Search */}
+
+    <EmojiSearch/>
+
+    </div>
+  );
+}
+
+export default App;
